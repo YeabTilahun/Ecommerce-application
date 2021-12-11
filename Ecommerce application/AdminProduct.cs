3cc182c66de71194624664db1423aa46d23822cc
KaleabSamuel <kaleabsamuel55@gmail.com>
2021-12-11 13:40:56 +0300

    admin page

---
+++ Ecommerce application/AdminProduct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ecommerce_application
+{
+    public partial class AdminProduct : Form
+    {
+        public AdminProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void AdminProduct_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTransactions_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
