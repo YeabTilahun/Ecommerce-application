ddfd416351bf56adb351fdb976c2f5573294beb2
kidussb <76646467+kidussb@users.noreply.github.com>
2021-12-17 22:07:37 -0800

    Maximum update

--- Ecommerce application/CustomerPage.Designer.cs
+++ Ecommerce application/CustomerPage.Designer.cs
@@ -37,16 +37,18 @@ namespace Ecommerce_application
             this.button4 = new System.Windows.Forms.Button();
             this.CloseM = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panelAdd = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.btnMaximized = new System.Windows.Forms.Button();
             this.btnRestore = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelAdd = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(121)))), ((int)(((byte)(99)))));
             this.panel1.Controls.Add(this.button8);
             this.panel1.Controls.Add(this.button3);
@@ -60,6 +62,7 @@ namespace Ecommerce_application
             // 
             // button8
             // 
+            this.button8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button8.BackColor = System.Drawing.Color.Transparent;
             this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button8.Image = global::Ecommerce_application.Properties.Resources.logout__1_;
@@ -110,6 +113,7 @@ namespace Ecommerce_application
             // 
             // CloseM
             // 
+            this.CloseM.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.CloseM.BackColor = System.Drawing.Color.Brown;
             this.CloseM.BackgroundImage = global::Ecommerce_application.Properties.Resources.close_tab3;
             this.CloseM.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
@@ -126,6 +130,8 @@ namespace Ecommerce_application
             // 
             // panel2
             // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.panel2.Controls.Add(this.btnMaximized);
             this.panel2.Controls.Add(this.btnRestore);
@@ -136,35 +142,13 @@ namespace Ecommerce_application
             this.panel2.Size = new System.Drawing.Size(814, 22);
             this.panel2.TabIndex = 26;
             // 
-            // panelAdd
-            // 
-            this.panelAdd.BackColor = System.Drawing.Color.White;
-            this.panelAdd.Location = new System.Drawing.Point(99, 22);
-            this.panelAdd.Name = "panelAdd";
-            this.panelAdd.Size = new System.Drawing.Size(814, 543);
-            this.panelAdd.TabIndex = 27;
-            this.panelAdd.Paint += new System.Windows.Forms.PaintEventHandler(this.panelAdd_Paint);
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Image = global::Ecommerce_application.Properties.Resources.minimize;
-            this.button1.Location = new System.Drawing.Point(726, 1);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(31, 22);
-            this.button1.TabIndex = 27;
-            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
-            // 
             // btnMaximized
             // 
             this.btnMaximized.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.btnMaximized.BackColor = System.Drawing.Color.Transparent;
             this.btnMaximized.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMaximized.Image = global::Ecommerce_application.Properties.Resources.Maximize_20px;
-            this.btnMaximized.Location = new System.Drawing.Point(756, 0);
+            this.btnMaximized.Location = new System.Drawing.Point(757, 0);
             this.btnMaximized.Name = "btnMaximized";
             this.btnMaximized.Size = new System.Drawing.Size(30, 25);
             this.btnMaximized.TabIndex = 32;
@@ -185,6 +169,32 @@ namespace Ecommerce_application
             this.btnRestore.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnRestore.UseVisualStyleBackColor = false;
             this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = global::Ecommerce_application.Properties.Resources.minimize;
+            this.button1.Location = new System.Drawing.Point(726, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 22);
+            this.button1.TabIndex = 27;
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panelAdd
+            // 
+            this.panelAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelAdd.BackColor = System.Drawing.Color.White;
+            this.panelAdd.Location = new System.Drawing.Point(99, 22);
+            this.panelAdd.Name = "panelAdd";
+            this.panelAdd.Size = new System.Drawing.Size(814, 543);
+            this.panelAdd.TabIndex = 27;
+            this.panelAdd.Paint += new System.Windows.Forms.PaintEventHandler(this.panelAdd_Paint);
             // 
             // CustomerPage
             // 
