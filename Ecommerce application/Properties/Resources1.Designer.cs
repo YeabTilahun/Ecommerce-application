5899e76f3f73e76e341083506a54df00e66c12a6
KaleabSamuel <kaleabsamuel55@gmail.com>
2022-01-02 15:36:03 +0300

    added 1 image

--- Ecommerce application/Properties/Resources1.Designer.cs
+++ Ecommerce application/Properties/Resources1.Designer.cs
@@ -843,6 +843,16 @@ namespace Ecommerce_application.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Asset_48 {
+            get {
+                object obj = ResourceManager.GetObject("Asset 48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Asset_49 {
             get {
                 object obj = ResourceManager.GetObject("Asset 49", resourceCulture);
