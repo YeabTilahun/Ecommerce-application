f829c55b4297a5f32125098b91d4a3b3f8eb555d
KaleabSamuel <kaleabsamuel55@gmail.com>
2022-01-27 22:20:45 +0300

    validating merchant

---
+++ Ecommerce application/ValidatingMerchant.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Ecommerce_application
+{
+    partial class ValidatingMerchant
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnDecline = new System.Windows.Forms.Button();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.picPermit = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPermit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnCancel.FlatAppearance.BorderSize = 0;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Location = new System.Drawing.Point(528, 649);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(130, 35);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // btnDecline
+            // 
+            this.btnDecline.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnDecline.FlatAppearance.BorderSize = 0;
+            this.btnDecline.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDecline.Location = new System.Drawing.Point(141, 649);
+            this.btnDecline.Name = "btnDecline";
+            this.btnDecline.Size = new System.Drawing.Size(130, 35);
+            this.btnDecline.TabIndex = 6;
+            this.btnDecline.Text = "Decline";
+            this.btnDecline.UseVisualStyleBackColor = false;
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnAccept.FlatAppearance.BorderSize = 0;
+            this.btnAccept.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAccept.Location = new System.Drawing.Point(5, 649);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(130, 35);
+            this.btnAccept.TabIndex = 5;
+            this.btnAccept.Text = "Accept";
+            this.btnAccept.UseVisualStyleBackColor = false;
+            // 
+            // picPermit
+            // 
+            this.picPermit.BackColor = System.Drawing.Color.Transparent;
+            this.picPermit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picPermit.Location = new System.Drawing.Point(3, 4);
+            this.picPermit.Name = "picPermit";
+            this.picPermit.Size = new System.Drawing.Size(655, 630);
+            this.picPermit.TabIndex = 4;
+            this.picPermit.TabStop = false;
+            // 
+            // ValidatingMerchant
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Ecommerce_application.Properties.Resources.Asset_3751;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(660, 700);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDecline);
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.picPermit);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ValidatingMerchant";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Validating Merchant";
+            ((System.ComponentModel.ISupportInitialize)(this.picPermit)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnDecline;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.PictureBox picPermit;
+    }
+}
