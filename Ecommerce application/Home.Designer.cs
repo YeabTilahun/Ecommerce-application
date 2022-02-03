066f0167db5cda02667d2a5b8773a3a9266ccde0
kidussb <76646467+kidussb@users.noreply.github.com>
2022-02-02 21:19:23 -0800

    Hover at Home "X" Button

--- Ecommerce application/Home.Designer.cs
+++ Ecommerce application/Home.Designer.cs
@@ -158,6 +158,8 @@ namespace Ecommerce_application
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
             // 
             // button2
             // 
