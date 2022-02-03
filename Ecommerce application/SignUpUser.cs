5f284e5e857f49e192f83aa132d6b82cf7bd7af2
yonatan-endale <88444847+yonatan-endale@users.noreply.github.com>
2022-02-03 10:06:32 +0300

    minor

--- Ecommerce application/SignUpUser.cs
+++ Ecommerce application/SignUpUser.cs
@@ -56,6 +56,7 @@ namespace Ecommerce_application
         {
             AdminDatabase check = new AdminDatabase();
             string[] user = check.GetAllUserName();
+            return user;
         }
     }
 }
